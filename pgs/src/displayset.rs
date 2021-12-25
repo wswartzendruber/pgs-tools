561efe7062bbd48b4052141183d7fbd2ed2f2966
William Swartzendruber <wswartzendruber@gmail.com>
2021-12-25 11:08:47 -0800

    Initial DisplaySet documentation.

--- pgs/src/displayset.rs
+++ pgs/src/displayset.rs
@@ -4,6 +4,31 @@
  * SPDX-License-Identifier: OSL-3.0
  */
 
+//! Operates on complete display sets (DS's).
+//!
+//! # Overview
+//!
+//! A display set (DS) is a collection of segments. Multiple DS's come together to form an
+//! epoch.
+//!
+//! A single DS will typically perform one of the following functions for the epoch it resides
+//! in:
+//!
+//! - Define and apply a new composition to the screen.
+//! - Redefine an existing composition (allows the player to seek directly to this DS).
+//! - Update a composition that is already on the screen.
+//! - Remove a composition from the screen.
+//!
+//! In this way, an epoch can be seen as using multiple DS entites to perform the following
+//! procedure:
+//!
+//! 1. Apply a composition to the screen.
+//! 2. Update it (if desired).
+//! 3. Remove it.
+//!
+//! As such, an epoch may simply be thought of as text that appears on the screen for a period
+//! of time.
+
 #[cfg(test)]
 mod tests;
 
@@ -16,17 +41,31 @@ pub use displaysetwrite::*;
 use std::collections::BTreeMap;
 use super::segment::{Crop, CompositionState, Sequence};
 
+/// Represents a complete display set (DS) within an epoch.
 #[derive(Clone, Debug, Default, Hash, PartialEq)]
 pub struct DisplaySet {
+    /// The timestamp indicating when composition decoding should start. In practice, this is
+    /// the time at which the composition is displayed.
     pub pts: u32,
+    /// The timestamp indicating when the composition should be displayed.
     pub dts: u32,
+    /// The width of the screen in pixels. This value should be consistent within a
+    /// presentation.
     pub width: u16,
+    /// The height of the screen in pixels. This value should be consistent within a
+    /// presentation.
     pub height: u16,
+    /// This value should be set to `0x10` but can otherwise be typically ignored.
     pub frame_rate: u8,
+    /// If set, indicates the ID of a preceding palette to be updated within the epoch.
     pub palette_update_id: Option<u8>,
+    /// The collection of windows referenced by this DS.
     pub windows: BTreeMap<u8, Window>,
+    /// The collection of palettes referenced by this DS.
     pub palettes: BTreeMap<Vid<u8>, Palette>,
+    /// The collection of objects referenced by this DS.
     pub objects: BTreeMap<Vid<u16>, Object>,
+    /// Defines the composition of objects into windows.
     pub composition: Composition,
 }
 
